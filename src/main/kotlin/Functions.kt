5c892fec7c33a1e0e770a0fd30e48eba75a24ea0
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-05-26 11:17:20 +0100

    starting with kotlin functions

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,15 @@
+/*
+ * Starting with functions:
+ * * to define:
+ * * * starting with syntax:
+ * fun + function name + ( in case => any parameters ) + ?type of return + { function body }.
+ */
+
+// defining new function
+fun sayHello():String{
+    return "Hello world"
+}
+fun main(){
+    println("Hi my name is Ilias I'm learning kotlin")
+    println(sayHello())
+}
