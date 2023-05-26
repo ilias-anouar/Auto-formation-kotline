216d8740303b6249674094b83a646c0544281ffb
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-05-26 12:14:39 +0100

    Update Functions.kt

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -6,10 +6,15 @@
  */
 
 // defining new function
+/*
+* setting the return value to string.
+* returning a message => Hello word.
+*/
 fun sayHello():String{
     return "Hello world"
 }
 fun main(){
     println("Hi my name is Ilias I'm learning kotlin")
+    //calling function sayHello().
     println(sayHello())
 }
