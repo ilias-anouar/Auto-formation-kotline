cdc2f26fa845f9c26336495099f75a5f05d9fb0b
ilias-anouar <115695877+ilias-anouar@users.noreply.github.com>
2024-02-22 12:29:56 +0000

    Update practice7.kt

--- src/main/kotlin/practice7.kt
+++ src/main/kotlin/practice7.kt
@@ -2,7 +2,7 @@ fun main() {
     val winningBid = Bid(5000, "Private Collector")
 
     println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
-    println("Item B is sold at ${auctionPrice(null, 3000)}.")
+    println("Item B is sold at ${auctionPrice(null, 10000)}.")
 }
 
 class Bid(val amount: Int, val bidder: String)
